e64ee795c21b674750fd9b2f43fa9df893a0ccc2
Yoonseop Kang <e0engoon@gmail.com>
2024-03-17 20:01:50 +0900

    Implement the body of /feed endpoint

--- catfeeder/app.py
+++ catfeeder/app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import aiohttp
+
+from catfeeder import secret
 
 app = FastAPI()
 
 
 @app.get("/feed")
-def feed():
-    return {"message": "feeded"}
+async def feed():
+    async with aiohttp.request("GET", secret.ENDPOINT) as response:
+        if response.status == 200:
+            # todo: write current time to database
+            return {"message": "feeded"}
+        else:
+            raise HTTPException(status_code=response.status)
