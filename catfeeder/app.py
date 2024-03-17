f6e2b64f58e95893997ee82e9c8fe87b474cb83f
Yoonseop Kang <e0engoon@gmail.com>
2024-03-17 21:03:49 +0900

    Log feeding times to a SQLite DB

--- catfeeder/app.py
+++ catfeeder/app.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI, HTTPException
+from contextlib import asynccontextmanager
+from datetime import datetime, UTC
+
 import aiohttp
+import aiosqlite
+from fastapi import FastAPI, HTTPException
 
 from catfeeder import secret
 
-app = FastAPI()
+
+async def create_db():
+    async with aiosqlite.connect(secret.DB_FILE) as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS feeds (time TEXT)")
+        await db.commit()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await create_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/feed")
 async def feed():
     async with aiohttp.request("GET", secret.ENDPOINT) as response:
         if response.status == 200:
-            # todo: write current time to database
-            return {"message": "feeded"}
+            now = datetime.now(tz=UTC).isoformat()
+            async with aiosqlite.connect(secret.DB_FILE) as db:
+                await db.execute("INSERT INTO feeds (time) VALUES (?)", (now,))
+            return {}
         else:
             raise HTTPException(status_code=response.status)
